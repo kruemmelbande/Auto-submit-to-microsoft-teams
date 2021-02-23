6c6668988a11e0098104a0d31c5bf33cb8f788ea
kruemmelbande <69861550+kruemmelbande@users.noreply.github.com>
2021-02-23 21:16:46 +0100

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,6 +1,15 @@
-import pyautogui
-import time
-import datetime
+try:
+        import time
+        import pyautogui
+        import datetime
+except:
+        print("An error occured.")
+        print("A package failed to load.")
+        print("pyautogui is likely missing. Run setup.bat. If that doesent work, try executing from comandline")
+        print("with 'python main.py' do not use 'main.py', as this seems to cause this.")
+        print("Exiting in 60 seconds.")
+        time.sleep(60)
+        print("Exiting....")
 print("Waiting untill pro gamer move")
 x=0
 while x==0:
